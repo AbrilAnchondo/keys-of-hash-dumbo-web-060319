a9b18da8571d013341c34e049ec43dd9f2fd0fd7
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-12 22:11:14 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,6 +3,8 @@ class Hash
     # code goes here
     array_of_keys = []
     self.each do |key, value|
+      #self gives acces to the current object-is the object that is #receiving the current message
+      #once I have acces to key and values of that hash, I can iterate #over arguments
       arguments.each do |arg|
         if arg == value
           array_of_keys << key
